a9d0b9abd459edbe664ce886043c38cc2ac9749d
Cupumendewa <cupumendewa@gmail.com>
2017-06-07 16:46:18 +0700

    Initial Commit.

---
+++ app/src/main/java/xir2n07/remidiuts/telkomschid/navigasi/MainActivity.java
@@ -0,0 +1,13 @@
+package xir2n07.remidiuts.telkomschid.navigasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
